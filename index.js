fa7de93202f8815707f6a095375e4e969e5fed6d
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-05-19 01:32:39 +0600

    category data created

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require("./data/categories.json");
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 
 app.listen(port, () => {
