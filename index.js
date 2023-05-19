590a37b202afe9d920a0c2be37dfad632e2bac96
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-05-19 11:44:58 +0600

    toydetails json created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const categories = require("./data/categories.json");
+const toyDetails = require("./data/toydetails.json");
 
 // middleware
 app.use(cors());
